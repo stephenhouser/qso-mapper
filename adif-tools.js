911539993fdb6b406b723d1f08d1082bde7ccc6d
Stephen Houser <stephenhouser@gmail.com>
2018-09-25 18:49:44 -0400

    split out files

---
+++ adif-tools.js
@@ -0,0 +1,79 @@
+
+function parseADIF(adifFileData) {
+	var qsos = [];
+
+	// remove all newlines from the file before parsing
+	cleanAdifData = adifFileData.replace(/(\r\n\t|\n|\r\t)/gm, '');
+
+	var filePartsMatch = cleanAdifData.match(/(.*)\<eoh>(.*)/);
+	if (filePartsMatch && filePartsMatch.length == 3) {
+		body = filePartsMatch[2];
+
+		qsoMatches = body.match(/(.*?)\<eor\>/g);
+		for (var q = 0; q < qsoMatches.length; q++) {
+			qso = parseADIFRecord(qsoMatches[q]);
+			qsos.push(qso);
+		}
+	} else {
+		throw 'Invalid ADIF file. Cannot find header marker <eoh>.';
+	}
+
+	return qsos;
+}
+
+// Series of `<name:len>data` fields
+function parseADIFRecord(record) {
+	var qso = {};
+
+	fields = record.match(/\<(.+?):.+?\>([^\<]+)/g);
+	for (var f = 0; f < fields.length; f++) {
+		var f_v = parseADIFField(fields[f]);
+		if (f_v !== null && f_v.length == 2) {
+			qso[f_v[0]] = f_v[1];
+		}
+	}
+
+	return qso;
+}
+
+// `<name:len>data`
+// <gridsquare:4>FN43
+function parseADIFField(field) {
+	// don't capture whitespace at end of field
+	var field_value = field.match(/\<(.+?):.+?\>(.*?)\s*$/);
+	if (field_value !== null) {
+		return [field_value[1], field_value[2]];
+	}
+
+	return null;
+}
+
+function parseCoordinate(dms) {
+	if (typeof (dms) !== 'string') {
+		throw "parseCoordinate: Invalid (null) coordinate passed.";
+	}
+
+	// Ex: N040 26.719
+	halfDecimalMatch = dms.match(/([NSEW])([0-9]+)\s+([0-9]*\.?[0-9]+)/);
+	if (halfDecimalMatch != null) {
+		var [_, direction, degrees, minutes] = halfDecimalMatch;
+		var dd = parseInt(degrees) + parseFloat(minutes) / 60;
+		if (direction == 'S' || direction == 'W') {
+			dd = -dd;
+		}
+
+		return dd;
+	}
+
+	// Ex: 43° 41' 11'' N     -70° 32' 60'' W 
+	fullDMSMatch = dms.match(/([+-][0-9]{1,2})°?\s+([0-9]{1,2})'?\s+([0-9]{1,2})(["']{1,2})?\s+([NSEW])/);
+	if (fullDMSMatch != null) {
+		var [_, degrees, minutes, seconds, _, direction] = coordinate.match(/([+-][0-9]{1,2})°?\s+([0-9]{1,2})'?\s+([0-9]{1,2})(["']{1,2})?\s+([NSEW])/);
+		var dd = parseInt(degrees) + (parseFloat(minutes) / 60) + (parseFloat(seconds) / 60 / 60);
+		if (direction == 'S' || direction == 'W') {
+			dd = -dd;
+		}
+
+		return dd;
+	}
+}
--- qsl-mapper.js
+++ qsl-mapper.js
@@ -19,43 +19,6 @@ function initMap() {
 	});
 }
 
-function addMarker(name, info, latitude, longitude) {
-	if (map !== null) {
-		let infowindow = new google.maps.InfoWindow({
-			content: info
-		});
-
-		let marker = new google.maps.Marker({
-			title: name,
-			position: new google.maps.LatLng(latitude, longitude),
-			map: map,
-		});
-
-		marker.addListener('click', function () {
-			if (openInfoWindow !== null) {
-				openInfoWindow.close();
-			}
-
-			infowindow.open(map, marker);
-			openInfoWindow = infowindow;
-		});
-
-		markers.push(marker);
-	}
-}
-
-function addMarkerForQSO(qso) {
-	var info = '<div class="qso">'+
-            '<h1>' + qso.call + '</h1>'+
-			'<div><p>'+
-			'Time:' + qso.qso_date + ' ' + qso.time_on +
-			' Band:' + qso.band +
-			' Mode:' + qso.mode +
-			'</p></div></div>';
-
-	addMarker(qso.call, info, parseCoordinate(qso.lat), parseCoordinate(qso.lon));
-}
-
 function clearMarkers() {
 	for (var i = 0; i < markers.length; i++) {
 		markers[i].setMap(null);
@@ -64,70 +27,94 @@ function clearMarkers() {
 	markers = [];
 }
 
-function parseADIFLine(qsoLine) {
-	var qso = {};
+function createInfoWindowForQSO(qso) {
+	var info = '<div class="qso">' +
+		'<h1>' + qso.call + '</h1>' +
+		'<div><p>' +
+		'Time:' + qso.qso_date + ' ' + qso.time_on +
+		' Band:' + qso.band +
+		' Mode:' + qso.mode +
+		'</p></div></div>';
 
-	fields = qsoLine.match(/\<(.+?):.+?\>([^\<]+)/g);
-	for (var f = 0; f < fields.length; f++) {
-		field_value = fields[f].match(/\<(.+?):.+?\>(.*)/);
-		if (field_value !== null) {
-			qso[field_value[1]] = field_value[2];
-		}
-	}
+	let infowindow = new google.maps.InfoWindow({
+		content: info
+	});
 
-	return qso;
+	return infowindow;
 }
 
-	//N040 26.719
-	//my_lat: "N043 40.871"
-	//43.686292, -70.549876
-	//43° 41' 11'' N     -70° 32' 60'' W 
-function parseCoordinate(coordinate) {
-	var [_, nsew, d, m] = coordinate.match(/([NSEW])(\d+) ([.\d]+)/);	
-	var dd = parseInt(d) + parseFloat(m) / 60;
-	if (nsew == 'S' || nsew == 'W') {
-		dd = -dd;
+function latLonForQso(qso) {
+	if (typeof (qso.lat) === 'string' && typeof (qso.lon) === 'string') {
+		var latitude = parseCoordinate(qso.lat);
+		var longitude = parseCoordinate(qso.lon);
+		return [latitude, longitude];
 	}
 
-	return dd;
+	if (typeof (qso.gridsquare) === 'string' 
+		&& (qso.gridsquare.length == 4 || qso.gridsquare.length == 6)) {
+		var [latitude, longitude] = latLonForGrid(qso.gridsquare);
+		return [latitude, longitude];
+	}
+
+	return null;
 }
 
-function loadADIFFile(file) {
-	var reader = new FileReader();
-	reader.onload = function (e) {
-		var fileData = e.target.result;
-		fileData = fileData.replace(/(\r\n\t|\n|\r\t)/gm, '');
+function createMarkerForQSO(qso) {
+	var latlon = latLonForQso(qso);
+	if (latlon === null) {
+		//throw "No location for qso";
+		return null;
+	}
 
-		var fileMatch = fileData.match(/(.*)\<eoh>(.*)/);
-		if (fileMatch && fileMatch.length == 3) {
-			body = fileMatch[2];
-			qsoMatches = body.match(/(.*?)\<eor\>/g);
-			for (var q = 0; q < qsoMatches.length; q++) {
-				qso = parseADIFLine(qsoMatches[q]);
+	var [latitude, longitude] = latlon;
 
-				addMarkerForQSO(qso);
+	let marker = new google.maps.Marker({
+		title: name,
+		position: new google.maps.LatLng(latitude, longitude)
+	});
 
-				if (qth === null) {
-					var qthInfo = '<div class="qth">' +
-						'<h1>' + qso.station_callsign + '</h1>' +
-						'<p>Station QTH</p>'
-						'</div>';
-					addMarker(qso.station_callsign, qthInfo, parseCoordinate(qso.my_lat), parseCoordinate(qso.my_lon));
-				}
-				// console.log(qso);
-			}
-		} else {
-			alert('Invalid ADIF file. Cannot find header marker <eoh>.');
+	marker.addListener('click', function () {
+		if (openInfoWindow !== null) {
+			openInfoWindow.close();
 		}
-	};
 
-	reader.readAsText(file);
+		let infowindow = createInfoWindowForQSO(qso);
+		infowindow.open(map, marker);
+		openInfoWindow = infowindow;
+	});
+
+	return marker;
+}
+
+function addMarkerForQSO(qso) {
+	var marker = createMarkerForQSO(qso);
+	if (marker) {
+		marker.setMap(map);
+		markers.push(marker);
+	}
+}
+
+function addMarkersForQSOs(qsos) {
+	for (var q = 0; q < qsos.length; q++) {
+		addMarkerForQSO(qsos[q]);
+	}
 }
 
 function handleFiles(files) {
 	var numFiles = files.length;
 	for (var i = 0, numFiles = files.length; i < numFiles; i++) {
-		var file = files[i];
-		loadADIFFile(file);
+		loadMarkersFromADIF(files[i], addMarkerForQSO);
 	}
+}
+
+function loadMarkersFromADIF(file, addMarkerFunc) {
+	var reader = new FileReader();
+	reader.onload = function (e) {
+		var qsos = parseADIF(e.target.result);
+		for (var q = 0; q < qsos.length; q++) {
+			addMarkerFunc(qsos[q]);
+		}
+	};
+
+	reader.readAsText(file);
 }
