ccd19e7a3c7adf0e10806c1cae7319bcf8e6d7e3
Stephen Houser <stephenhouser@gmail.com>
2018-09-30 13:01:01 -0400

    Enable mapping adif file from URL

--- leaflet-map.js
+++ leaflet-map.js
@@ -1,7 +1,7 @@
-// *** *** Map Functions *** ***
-
-var _map = null;
-var _markers = [];
+/* Leaflet JS Map Functions (https://leafletjs.com)
+ *
+ * Using OpenStreetMap (https://www.openstreetmap.org)
+ */
 
 /* Required Map functions:
  *
@@ -11,14 +11,22 @@ var _markers = [];
  * removeAllMarkers()
  */
 
+/* Internal variables */
+var _map = null;
+var _markers = [];
 var mapAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 	'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 	'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
-// var mapTileFormat = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 var mapTileFormat = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+// For https://mapbox.com use the followng
+// var mapTileFormat = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={access_token}'
 
-
+/* createMap - Initialize and create map in named section of DOM
+ * 
+ * Set up any handlers needed by the map and perform any initialization
+ * of the map library.
+ */
 function createMap(mapDivName) {
 	_map = L.map(mapDivName).setView([43.686292, -70.549876], 10);
 
@@ -30,6 +38,11 @@ function createMap(mapDivName) {
 	return _map;
 }
 
+/* createMarker - create a marker on the map at the given position.
+ *
+ * Set up a marker on the map and corresponding pop-up for when the
+ * marker is selected.
+ */
 function createMarker(latitude, longitude, popupText) {
 	var marker = L.marker([latitude, longitude])
 	marker.addTo(_map)
@@ -39,6 +52,8 @@ function createMarker(latitude, longitude, popupText) {
 	return marker;
 }
 
+/* removeMarker - remove the given marker from the map.
+ */
 function removeMarker(marker) {
 	if (marker !== null) {
 		var markerIndex = _markers.indexOf(marker);
@@ -50,6 +65,8 @@ function removeMarker(marker) {
 	}
 }
 
+/* removeAllMarkers - remove all markers from the map
+ */
 function removeAllMarkers() {
 	while (_markers.length > 0) {
 		var marker = _markers.pop();
--- qso-mapper.js
+++ qso-mapper.js
@@ -5,10 +5,31 @@
 // 	alert('The File APIs are not fully supported in this browser.');
 // }
 
+/*
+TODO: Enable loading ADIF files from URL query string and saving in local storage
+
+1. Use AJAX to get file from same repo as passed from URL
+	https://stephenhouser.com/qso-mapper?short.adi
+	https://stephenhouser.com/qso-mapper?https://stephenhouser.com/qso-mapper/short.adi
+	- don't use any local storage
+	- should the file upload be available when this is the case? prob not.
+	- disable reset button and file upload
+
+2. Use data uploaded by the file upload form
+	- save in local storage
+*/
+
+/* Names of HTML DOM elements */
 var mapElementName = 'map-canvas';
 var fileUploadFormName = 'fileUpload';
-var fileInputFieldName = 'fileInput'
+var fileInputFieldName = 'fileInput';
+var resetButtonName = 'resetMarkers';
 
+/* initQsoMapper - main initialization for page
+ *
+ * - Creates and loads map element
+ * - Attaches page event handlers
+ */
 function initQsoMapper() {
 	createMap(mapElementName);
 
@@ -16,10 +37,39 @@ function initQsoMapper() {
 	document.getElementById(fileInputFieldName).addEventListener('change', handleUploadFile);
 
 	// Call removeAllMarkers when 'Reset' is clicked and reset fileName
-	document.getElementById('resetMarkers').addEventListener('click', handleResetUpload);
+	document.getElementById(resetButtonName).addEventListener('click', handleResetUpload);
+
+	// If a url is specified in the query string, try to load and show markers from it
+	// This allows pre-coding a URL with the ADIF file
+	// https://stephenhouser.com/qso-mapper?url=sample/short.adi
+	// https://stephenhouser.com/qso-mapper?url=https://stephenhouser.com/qso-mapper/sample/short.adi
+	var loadURL = getQueryVariable('url');
+	if (loadURL !== null) {
+		requestADIFFromURL(loadURL);
+		disableFileUpload();
+	}
 }
 
-// handle when upload file is triggered
+/* disableFileUpload - disable the file upload form and reset buttons
+ *
+ * - Updates the file upload form (with the current file name)
+ * - Checks and parses the file, loading QSOs into an array
+ * - Adds markers (from QSOs) to the map
+ */
+function disableFileUpload() {
+	var fileUploadForm = document.getElementById(fileUploadFormName);
+	fileUploadForm.hidden = true;
+
+	var resetButton = document.getElementById(resetButtonName);
+	resetButton.hidden = true;
+}
+
+/* handleFileUpload - handle when upload file is triggered
+ *
+ * - Updates the file upload form (with the current file name)
+ * - Checks and parses the file, loading QSOs into an array
+ * - Adds markers (from QSOs) to the map
+ */
 function handleUploadFile() {
 	var fileName = this.value;
 	fileName = fileName.replace('C:\\fakepath\\', '');
@@ -28,13 +78,21 @@ function handleUploadFile() {
 	uploadFiles(this.files);
 }
 
-// handle pressing of the 'reset' button
+/* handleResetUpload - handle pressing of the 'reset' button
+ *
+ * - Removes any markers from the map
+ * - Resets the file upload form
+ */
 function handleResetUpload() {
 	setFileInputLabel('Select file...');
 	removeAllMarkers();
 }
 
-// set the current 'uploaded' file name
+/* setFileInputLabel - set the current 'uploaded' file name
+ *
+ * Utility function to set the last loaded file name on the file upload
+ * form.
+ */
 function setFileInputLabel(message) {
 	var fileUploadForm = document.getElementById(fileUploadFormName);
 	var fileLabels = fileUploadForm.getElementsByClassName('custom-file-label');
@@ -43,26 +101,28 @@ function setFileInputLabel(message) {
 	}
 }
 
-// upload/process files
+/* uploadFiles - loads QSOs from uploaded files */
 function uploadFiles(files) {
 	var numFiles = files.length;
 	for (var i = 0, numFiles = files.length; i < numFiles; i++) {
-		loadMarkersFromADIF(files[i], addMarkerForQso);
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			loadQsosFromADIF(e.target.result, addMarkerForQso);
+		};
+
+		reader.readAsText(files[i]);
 	}
 }
 
-function loadMarkersFromADIF(file, addMarkerFunc) {
-	var reader = new FileReader();
-	reader.onload = function (e) {
-		var qsos = parseADIF(e.target.result);
-		for (var q = 0; q < qsos.length; q++) {
-			addMarkerFunc(qsos[q]);
-		}
-	};
-
-	reader.readAsText(file);
+/* loadQsosFromADIF - load QSOs from a single ADIF file */
+function loadQsosFromADIF(adifString, addMarkerFunc) {
+	var qsos = parseADIF(adifString);
+	for (var q = 0; q < qsos.length; q++) {
+		addMarkerFunc(qsos[q]);
+	}
 }
 
+/* addMarkerForQso - add a marker to the map for a given QSO */
 function addMarkerForQso(qso) {
 	var latlon = latLonForQso(qso);
 	if (latlon === null) {
@@ -72,7 +132,7 @@ function addMarkerForQso(qso) {
 
 	var [latitude, longitude] = latlon;
 	var popupText = '<div class="qso">' +
-		'<h1>' + qso.call + '</h1>' +
+		'<h1>' + qso.call + '</h1><hr/>' +
 		'<div><p>' +
 		'Time:' + qso.qso_date + ' ' + qso.time_on +
 		' Band:' + qso.band +
@@ -83,6 +143,14 @@ function addMarkerForQso(qso) {
 	return marker;
 }
 
+/* latLonForQSO - get the latitude and longitude for a QSO
+ *
+ * A QSO *may* already contain a latitide and longitude. If so, use those
+ * values. Otherwise, if the QSO contains a grid square (maidenhead locator),
+ * use the center of the square as the latitude and longitude.
+ * This makes use of the HamGridSquare.js:
+ * 	https://gist.github.com/stephenhouser/4ad8c1878165fc7125cb547431a2bdaa
+ */
 function latLonForQso(qso) {
 	if (typeof (qso.lat) === 'string' && typeof (qso.lon) === 'string') {
 		var latitude = parseCoordinate(qso.lat);
@@ -97,4 +165,44 @@ function latLonForQso(qso) {
 	}
 
 	return null;
+}
+
+function getQueryVariable(variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		if (decodeURIComponent(pair[0]) == variable) {
+			return decodeURIComponent(pair[1]);
+		}
+	}
+
+	return null;
+}
+
+function requestADIFFromURL(url) {
+	var httpRequest;
+	function makeRequest(url) {
+		httpRequest = new XMLHttpRequest();
+
+		if (!httpRequest) {
+			alert('Giving up :( Cannot create an XMLHTTP instance');
+			return false;
+		}
+		httpRequest.onreadystatechange = alertContents;
+		httpRequest.open('GET', url);
+		httpRequest.send();
+	}
+
+	function alertContents() {
+		if (httpRequest.readyState === XMLHttpRequest.DONE) {
+			if (httpRequest.status === 200) {
+				loadQsosFromADIF(httpRequest.responseText, addMarkerForQso);
+			} else {
+				alert('There was a problem with the request.');
+			}
+		}
+	}
+
+	makeRequest(url);
 }
